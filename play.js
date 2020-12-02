7593e5d00ea6de8413aa773004240b4a8b8847db
Alan <alanmak95@gmail.com>
2020-12-02 19:41:54 +0000

    Added the Ctrl + C output function

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
-const {connect} = require('./client');
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+// Used for user input
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  //Handling Ctrl + C
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on('data', handleUserInput)
+  return stdin;
+}
+setupInput();
+
