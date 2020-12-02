108e9d192c5fe31ec8d5c798303e2353812beb86
Alan <alanmak95@gmail.com>
2020-12-02 19:12:59 +0000

    Added name to snake and connected to game message

--- client.js
+++ client.js
@@ -8,8 +8,13 @@ const connect = function() {
     host: '135.23.222.131',
     port: 50542
   });
+  // Message right after connecting to servier
+  conn.on('connect', () => console.log("Successfully connected to game server"));
+  // Snake display name
+  conn.on('connect', () => conn.write("Name: MAK"));
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  // interrpret messages from the server
   conn.on('data', data => console.log('Server Says:', data))
   return conn;
 }
