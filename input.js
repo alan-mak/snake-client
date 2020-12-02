3a95be7a62edb284acee5c4cf9a98655a89ec9b5
Alan <alanmak95@gmail.com>
2020-12-02 20:09:44 +0000

    Took the setupInput function into a new file

---
+++ input.js
@@ -0,0 +1,17 @@
+// Used for user input
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  //Handling Ctrl + C
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on('data', handleUserInput)
+  return stdin;
+}
+
+module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input')
+
 console.log('Connecting ...');
 connect();
-
-// Used for user input
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  //Handling Ctrl + C
-  const handleUserInput = function (data) {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  }
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
 setupInput();
 
